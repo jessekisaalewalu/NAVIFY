5d717d1e2ac621883ce5dd0325b7c7ccaca2bcf2
jessekisaalewalu <princejesse009@gmail.com>
2025-12-11 15:23:15 +0300

    Deploy NAVIFY client to GitHub Pages

--- server/controllers/adminController.js
+++
@@ -1,5 +0,0 @@
-// adminController.js - deprecated
-// The admin summary feature has been removed from the API and client UI.
-// Keeping this stub to avoid accidental requires causing runtime errors during incremental refactors.
-
-module.exports = {};
